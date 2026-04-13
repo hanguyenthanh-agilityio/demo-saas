ca1993950ba62d3691088752e7798e02e7e0287b
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2026-04-14 06:45:18 +0700

    feat: update login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from "../fixtures/login-fixture";
 
 // Pages
 import { DashboardPage } from "../pages/dashboard";
+import { LoginCase } from "../types/login";
 
 // Env
 import { ENV } from "../utils/env";
@@ -37,13 +38,15 @@ test.describe("Login Feature - SaaS", () => {
   // ======================
   // NEGATIVE CASES
   // ======================
-  const cases = [
+  const cases: LoginCase[] = [
     {
       id: "TC002",
       desc: "Verify error when email is empty",
       email: "",
       password: ENV.PASSWORD,
       type: "ui",
+      field: "email",
+      errorMess: "Invalid email",
     },
     {
       id: "TC003",
@@ -51,6 +54,8 @@ test.describe("Login Feature - SaaS", () => {
       email: ENV.EMAIL,
       password: "",
       type: "ui",
+      field: "global",
+      errorMess: "Invalid email or password",
     },
     {
       id: "TC004",
@@ -58,6 +63,8 @@ test.describe("Login Feature - SaaS", () => {
       email: "abc",
       password: ENV.PASSWORD,
       type: "ui",
+      field: "email",
+      errorMess: "Invalid email",
     },
     {
       id: "TC005",
@@ -65,16 +72,10 @@ test.describe("Login Feature - SaaS", () => {
       email: ENV.EMAIL,
       password: "wrong123",
       type: "api",
-    },
-    {
-      id: "TC006",
-      desc: "Verify error when email is not registered",
-      email: "wrong@gmail.com",
-      password: ENV.PASSWORD,
-      type: "api",
+      field: "global",
+      errorMess: "Invalid email or password",
     },
   ];
-
   for (const c of cases) {
     test(
       `${c.id} - ${c.desc}`,
@@ -97,7 +98,7 @@ test.describe("Login Feature - SaaS", () => {
         }
 
         await test.step("Verify error message displayed", async () => {
-          await loginPage.expectErrorMessage();
+          await loginPage.expectErrorMessage(c.errorMess, c.field);
         });
 
         await test.step("Verify still on login page", async () => {
--- types/login.ts
+++ types/login.ts
@@ -9,3 +9,15 @@ export type LoginFixtures = {
 export type LoginResponse = {
   status: () => number;
 };
+
+export type LoginErrorField = "email" | "password" | "global";
+
+export type LoginCase = {
+  id: string;
+  desc: string;
+  email: string;
+  password: string;
+  type: "ui" | "api";
+  field?: LoginErrorField;
+  errorMess?: string;
+};
